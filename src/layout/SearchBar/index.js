188d40af1c5e59712bc4321e3a0e40b16885d9a5
ShinyKiwis <nguyen@DESKTOP-PSMANG4.localdomain>
2022-09-10 23:45:54 +0700

    Update: Finish basic layout

--- src/layout/SearchBar/index.js
+++ src/layout/SearchBar/index.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import SearchBarStyle from "./SearchBar.module.css";
 import { BsSearch } from "react-icons/bs";
+import { GoSettings } from "react-icons/go";
 
 const Filter = () => {
-    return (
-        <div>
-            Filter
-        </div>
-    )
+  return (
+    <div className={SearchBarStyle.filter_container}>
+      <GoSettings className={SearchBarStyle.filter_icon} />
+    </div>
+  );
+};
+
+const FilterItem = ({ title, selectedFilter, setSelectedFilter }) => {
+  const pressHandle = () => {
+    if (selectedFilter.includes(title)) {
+      setSelectedFilter((selectedFilter) =>
+        selectedFilter.filter((filter) => filter != title)
+      );
+    } else {
+      setSelectedFilter([...selectedFilter, title]);
+    }
+  };
+  return (
+    <span
+      onClick={pressHandle}
+      className={
+        selectedFilter.includes(title)
+          ? SearchBarStyle.selected_filter_option
+          : SearchBarStyle.filter_option
+      }
+    >
+      {title}
+    </span>
+  );
 };
 
 const SearchInput = () => {
@@ -23,11 +49,24 @@ const SearchInput = () => {
 };
 
 const SearchBar = () => {
+  const [selectedFilter, setSelectedFilter] = useState([]);
+  const filterOptions = ["Place", "Bus", "Bike", "Theaters", "Markets"];
   return (
-    <div className={SearchBarStyle.search_container}>
-      <SearchInput />
-      <Filter />
-    </div>
+    <>
+      <div className={SearchBarStyle.search_container}>
+        <SearchInput />
+        <Filter />
+      </div>
+      <div className={SearchBarStyle.filter_options_container}>
+        {filterOptions.map((option) => (
+          <FilterItem
+            title={option}
+            selectedFilter={selectedFilter}
+            setSelectedFilter={setSelectedFilter}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
